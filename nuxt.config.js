75af18be5f1a5908fbcd7e124238f9df318f4f8f
jessylinlin <1501103995@qq.com>
2021-07-28 13:30:33 +0800

    3000

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,51 +9,47 @@ module.exports = {
             routes.splice(0)
 
             routes.push(...[{
-                path: '/',
-                component: resolve(__dirname, 'pages/layout/'),
-                children: [{
-                    path: '', //默认子路由
-                    name: 'home',
-                    component: resolve(__dirname, 'pages/home/')
-                },
-                {
-                    path: '/login',
-                    name: 'login',
-                    component: resolve(__dirname, 'pages/login/')
-                },
-                {
-                    path: '/register',
-                    name: 'register',
-                    component: resolve(__dirname, 'pages/login/')
-                },
-                {
-                    path: '/profile/:username',
-                    name: 'profile',
-                    component: resolve(__dirname, 'pages/profile/')
-                },
-                {
-                    path: '/settings',
-                    name: 'settings',
-                    component: resolve(__dirname, 'pages/settings/')
-                },
-                {
-                    path: '/editor',
-                    name: 'editor',
-                    component: resolve(__dirname, 'pages/editor/')
-                },
-                {
-                    path: '/article/:slug',
-                    name: 'article',
-                    component: resolve(__dirname, 'pages/article/')
-                }
-                ]
-            }])
-            // console.log(routes)
+                    path: '/',
+                    component: resolve(__dirname, 'pages/layout/'),
+                    children: [{
+                            path: '', //默认子路由
+                            name: 'home',
+                            component: resolve(__dirname, 'pages/home/')
+                        },
+                        {
+                            path: '/login',
+                            name: 'login',
+                            component: resolve(__dirname, 'pages/login/')
+                        },
+                        {
+                            path: '/register',
+                            name: 'register',
+                            component: resolve(__dirname, 'pages/login/')
+                        },
+                        {
+                            path: '/profile/:username',
+                            name: 'profile',
+                            component: resolve(__dirname, 'pages/profile/')
+                        },
+                        {
+                            path: '/settings',
+                            name: 'settings',
+                            component: resolve(__dirname, 'pages/settings/')
+                        },
+                        {
+                            path: '/editor',
+                            name: 'editor',
+                            component: resolve(__dirname, 'pages/editor/')
+                        },
+                        {
+                            path: '/article/:slug',
+                            name: 'article',
+                            component: resolve(__dirname, 'pages/article/')
+                        }
+                    ]
+                }])
+                // console.log(routes)
         }
-    },
-    server: {
-        host: '0.0.0.0', //默认localhost
-        port: 8000
     },
     //注册插件
     plugins: [
